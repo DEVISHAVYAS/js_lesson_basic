c7a2e36e7b823381ff2844656d189d5fd9fd1ef0
Dev19820810 <155079184+Dev19820810@users.noreply.github.com>
2023-12-29 21:17:46 -0500

    conversion of operater

--- 03_conversionoperation.js
+++ 03_conversionoperation.js
@@ -1,4 +1,5 @@
-let score = "33" //print number without the double quote
+//let score = "33xyz" //print number without the double quote
+let score = null  // ans is 0 
 //with double quote it shows on terminal ans string
 console.log(typeof score);  //print number
 console.log(typeof(score)); //print number
@@ -6,3 +7,19 @@ console.log(typeof(score)); //print number
 let valueInNumber= Number(score)    //score
 console.log(typeof valueInNumber); //number
 console.log(valueInNumber);        //33
+
+//boolen true =>1;
+//false =>0;
+//let isLoggedIn = 1;       //Ans is true
+//let isLoggedIn ="Devisha"
+let isLoggedIn = "";       //Ans is false
+let boolenIsLoggedIn = Boolean(isLoggedIn)
+//console.log(boolenIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"Devisha" =>true
+
+let someNumber = 55
+let stringNumber = String(someNumber)
+console.log(someNumber);                  //Ans is 55
